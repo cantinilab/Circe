939b5858e5c44fe50b9f04b2e4505c1490facf90
r-trimbour <trimbour@edu.bio.ens.psl.eu>
2024-01-09 15:40:04 +0100

    Correct alpha calculation

--- atac_networks/atac_networks.py
+++ atac_networks/atac_networks.py
@@ -160,14 +160,14 @@ def local_alpha(
         mask_distance = distances > distance_constraint
         far_edges = (
             results[mask_distance] != 0).sum() / len(results[mask_distance])
-        print(results[mask_distance])
+        print(results[mask_distance].shape)
         print(len(results[mask_distance]))
         print(far_edges)
 
         near_edges = (results != 0).sum() / (results.shape[0] ** 2)
         # If far_edges is too high (not sparse enough after filtering),
         #  increase distance_parameter
-        if far_edges > 0.05 or near_edges > 0.8 or distance_parameter == 0:
+        if (far_edges < 0.05 and near_edges > 0.2) or distance_parameter == 0:
             distance_parameter_min = distance_parameter
         # If far_edges is too low (too sparse because of filtering),
         #  decrease distance_parameter
