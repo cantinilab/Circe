be4e6d14fd7ec97177e874dfe3d49f3148d3361a
r-trimbour <trimbour@edu.bio.ens.psl.eu>
2024-01-18 18:44:51 +0100

    add saprse_matrix for sliding_graphical lasso

--- atacnet/atacnet.py
+++ atacnet/atacnet.py
@@ -584,10 +584,16 @@ def sliding_graphical_lasso(
                 # idx = [map_indices[i] for i in regions_list[idx]]
 
                 # Get submatrix
-                window_accessibility = AnnData.X[:, idx].copy()
-                window_scores = np.cov(window_accessibility, rowvar=False)
-                window_scores = window_scores + 1e-4 * np.eye(
-                    len(window_scores))
+                if sp.sparse.issparse(AnnData.X):
+                    window_accessibility = AnnData.X[idx, :].toarray().copy()
+                    window_scores = np.cov(window_accessibility, rowvar=False)
+                    window_scores = window_scores + 1e-4 * np.eye(
+                        len(window_scores))
+                else:
+                    window_accessibility = AnnData.X[:, idx].copy()
+                    window_scores = np.cov(window_accessibility, rowvar=False)
+                    window_scores = window_scores + 1e-4 * np.eye(
+                        len(window_scores))
 
                 distance = get_distances_regions(AnnData[:, idx])
                 # Test if distance is negative
