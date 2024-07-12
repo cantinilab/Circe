26e3b405d3255b0d8a19245f259468834235a0b6
r-trimbour <trimbour@edu.bio.ens.psl.eu>
2024-07-12 17:38:04 +0200

    store code into src folder

--- circe/__init__.py
+++
@@ -1,7 +0,0 @@
-from .circe import extract_atac_links, \
-    compute_atac_network, \
-    add_region_infos, \
-    sliding_graphical_lasso, \
-    subset_region
-
-from .draw import plot_connections
