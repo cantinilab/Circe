de026fc0163b644ab5eb84dbbc9999348d63b0e7
Rémi Trimbour <77151348+r-trimbour@users.noreply.github.com>
2024-11-21 12:13:53 +0100

    tqdm description correction

--- circe/circe.py
+++ circe/circe.py
@@ -1089,7 +1089,7 @@ def chr_batch_graphical_lasso(
                 unit_distance,
                 init_method,
                 map_indices)
-            for idx in tqdm.tqdm(idxs, position=n, leave=False, f"Processing chromosome: '{chromosme}'"))
+            for idx in tqdm.tqdm(idxs, position=n, leave=False, desc=f"Processing chromosome: '{chromosme}'"))
 
         # Unpack the results and concatenate the arrays
         scores_list, idx_list, idy_list = zip(*parallel_lasso_results)
