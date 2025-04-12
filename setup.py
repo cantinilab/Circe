a1eee9b39a30653acb3c082b9f89834d01c52ae9
r-trimbour <trimbour@edu.bio.ens.psl.eu>
2025-04-13 00:07:19 +0200

    bump to 0.3.6 to match new version of pypi, reming installation through wheel. No change otherwise

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ extensions = [
 
 setup(
     name='circe-py',
-    version='0.3.5',
+    version='0.3.6',
     description='Circe: Package for building co-accessibility networks from ATAC-seq data.',
     long_description="None",
     author='Remi-Trimbour',
