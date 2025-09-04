6c4db60f6858eb90aadea608ca2432044439c188
Rémi Trimbour <77151348+r-trimbour@users.noreply.github.com>
2025-09-04 17:29:33 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 
 html_title = "CIRCE"
 html_static_path = ['_static']
