3bdd8b33f8e7995d8d6d9e183be6aa47ab8e78b5
Rémi Trimbour <77151348+r-trimbour@users.noreply.github.com>
2025-09-07 23:25:18 +0200

    Update sphinx root path to be recognised by rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,17 @@ copyright = '2025, Remi Trimbour'
 author = 'Remi Trimbour'
 release = '0.3.8'
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
